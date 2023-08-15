5be7f1231b4cf989c7c910c5627556b4ad720901
palak <goelpalak003@gmail.com>
2023-08-15 16:30:31 +0530

    feat: Added fuzzy search logic

--- mongodbatlas/src/main/java/com/example/mongodbatlas/repository/SearchFoodImpl.java
+++ mongodbatlas/src/main/java/com/example/mongodbatlas/repository/SearchFoodImpl.java
@@ -1,12 +1,38 @@
 package com.example.mongodbatlas.repository;
 
 import com.example.mongodbatlas.entity.FoodItems;
-
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SearchFoodImpl implements SearchFoodRepository{
+@Component
+public class SearchFoodImpl implements SearchFoodRepository {
+
+    @Autowired
+    MongoClient mongoClient;
+    @Autowired
+    MongoConverter mongoConverter;
+
     @Override
     public List<FoodItems> findByText(String text) {
-        return null;
+        List<FoodItems> foodItems = new ArrayList<>();
+        MongoDatabase database = mongoClient.getDatabase("fooditems");
+        MongoCollection<Document> collection = database.getCollection("foodItem");
+        AggregateIterable<Document> result = collection.aggregate(List.of(new Document("$search",
+                new Document("index", "default")
+                        .append("autocomplete", new Document("query", text)
+                                .append("path", "name")
+                                .append("tokenOrder", "any")
+                                .append("fuzzy", new Document("maxEdits", 1L)
+                                        .append("prefixLength", 2L))))));
+        result.forEach(doc-> foodItems.add(mongoConverter.read(FoodItems.class,doc)));
+        return foodItems;
     }
 }
