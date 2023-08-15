5261170412199e2f28ecbf70768b7899e61f7815
palak <goelpalak003@gmail.com>
2023-08-15 16:18:46 +0530

    init: Initialised project structure

---
+++ mongodbatlas/src/main/java/com/example/mongodbatlas/MongodbatlasApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbatlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbatlasApplication.class, args);
+	}
+
+}
